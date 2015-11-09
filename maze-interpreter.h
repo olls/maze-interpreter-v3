024c8525f3772abb68810536dcc7b966d7eab19e
Oliver Faircliff <faircliff.oliver@gmail.com>
2015-11-09 15:28:04 +0000

    Fixed #1

--- maze-interpreter.h
+++ maze-interpreter.h
@@ -13,19 +13,19 @@
 #define invalid_code_path assert(!"Invalid Code Path! D:")
 
 
-#define kilobytes_to_bytes(n) (1024 * n)
-#define megabytes_to_bytes(n) (kilobytes_to_bytes(1024) * n)
-#define gigabytes_to_bytes(n) (megabytes_to_bytes(1024) * n)
+#define kilobytes_to_bytes(n) (1024 * (n))
+#define megabytes_to_bytes(n) (kilobytes_to_bytes(1024) * (n))
+#define gigabytes_to_bytes(n) (megabytes_to_bytes(1024) * (n))
 
-#define bytes_to_kilobytes(n) (n / 1024)
-#define bytes_to_megabytes(n) (n / kilobytes_to_bytes(1024))
-#define bytes_to_gigabytes(n) (n / bytes_to_megabytes(1024))
+#define bytes_to_kilobytes(n) ((n) / 1024)
+#define bytes_to_megabytes(n) ((n) / kilobytes_to_bytes(1024))
+#define bytes_to_gigabytes(n) ((n) / bytes_to_megabytes(1024))
 
-#define seconds_in_m(n) (1000 * n )
-#define seconds_in_u(n) (seconds_in_m(1000) * n )
+#define seconds_in_m(n) (1000 * (n))
+#define seconds_in_u(n) (seconds_in_m(1000) * (n))
 
-#define m_in_seconds(n) (n / (real32)seconds_in_m(1))
-#define u_in_seconds(n) (n / (real32)seconds_in_u(1))
+#define m_in_seconds(n) ((n) / (real32)seconds_in_m(1))
+#define u_in_seconds(n) ((n) / (real32)seconds_in_u(1))
 
 #define array_count(array) (sizeof(array) / sizeof((array)[0]))
 
