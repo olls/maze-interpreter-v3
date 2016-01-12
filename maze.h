652c8691cbd44c7f169cc90fb8524ae3ab993baf
Oliver Faircliff <faircliff.oliver@gmail.com>
2016-01-12 21:29:42 +0000

    Fix function parsing

--- maze.h
+++ maze.h
@@ -47,18 +47,36 @@ struct QuadTree
 
 enum FunctionType
 {
-  FunctionAssignment,
-  FunctionIncrement,
-  FunctionDecrement,
-  FunctionConditional
+  FUNCTION_ASSIGNMENT,
+  FUNCTION_INCREMENT,
+  FUNCTION_DECREMENT,
+  FUNCTION_MULTIPLY,
+  FUNCTION_DIVIDE,
+  FUNCTION_LESS,
+  FUNCTION_LESS_EQUAL,
+  FUNCTION_EQUAL,
+  FUNCTION_NOT_EQUAL,
+  FUNCTION_GREATER_EQUAL,
+  FUNCTION_GREATER,
+  FUNCTION_SIGNAL
 };
 
 
 const uint32_t MAX_FUNCTIONS = (26*2) * ((26*2) + 10); // [A-z][A-z0-9]
 struct Function
 {
-  FunctionType type;
   char name[2];
+  FunctionType type;
+  union
+  {
+    uint32_t value;
+    struct
+    {
+      uint32_t condition_value;
+      Direction true_direction;
+      Direction false_direction;
+    } conditional;
+  };
 };
 
 
--- parser.cpp
+++ parser.cpp
@@ -2,6 +2,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define consume_whitespace(ptr, end) while ((*(ptr) == ' ' || *(ptr) == '\t') && (ptr) < end) ++(ptr)
+#define consume_whitespace_nl(ptr, end) while ((*(ptr) == ' ' || *(ptr) == '\t' || isNewline(*(ptr))) && (ptr) < end) ++(ptr)
+#define consume_until_newline(ptr, end) while (!isNewline(*(ptr)) && (ptr) < end) ++(ptr)
+#define consume_while(ptr, func, end) while (func(*(ptr)) && (ptr) < end) ++(ptr)
+
 
 bool
 isNum(char character)
@@ -16,6 +21,14 @@ isLetter(char character)
 {
   bool result = (((character >= 'a') && (character <= 'z')) ||
                  ((character >= 'A') && (character <= 'Z')));
+  return result;
+}
+
+
+bool
+isNewline(char character)
+{
+  bool result = (character == '\n') || (character == '\r');
   return result;
 }
 
@@ -50,6 +63,69 @@ get_function_index(char name[2])
 
   assert(result < MAX_FUNCTIONS);
   return result;
+}
+
+
+char *
+get_num(char * ptr, char * f_end, uint32_t * result)
+{
+  char * num_start = ptr;
+
+  consume_while(ptr, isNum, f_end);
+
+  *result = 0;
+  uint32_t num_length = ptr - num_start;
+
+  for (uint32_t num_pos = 0;
+       num_pos < num_length;
+       ++num_pos)
+  {
+    uint32_t digit = *(num_start + num_pos) - '0';
+    *result += digit * pow(10, (num_length - num_pos - 1));
+  }
+
+  return ptr;
+}
+
+
+char *
+get_direction(char * ptr, enum Direction * result)
+{
+  *result = STATIONARY;
+
+  if (ptr[0] == '%')
+  {
+    switch (ptr[1])
+    {
+      case 'U':
+      case 'u':
+      {
+        *result = UP;
+      } break;
+      case 'D':
+      case 'd':
+      {
+        *result = DOWN;
+      } break;
+      case 'L':
+      case 'l':
+      {
+        *result = LEFT;
+      } break;
+      case 'R':
+      case 'r':
+      {
+        *result = RIGHT;
+      } break;
+      default:
+      {
+        return ptr;
+      }
+    }
+    ptr += 2;
+  }
+
+  return ptr;
 }
 
 
@@ -122,24 +198,184 @@ parse(GameMemory * game_memory, const char * filename)
     }
     else if (isLetter(cell_str[0]) && (isLetter(cell_str[1]) || isNum(cell_str[1])))
     {
-      // uint32_t function_index = get_function_index(cell_str);
-      // uint32_t function_parse_bytes_read = 0;
-      // if ((sscanf((cell_str + offset + bytes_read), " -> %n", &function_parse_bytes_read) != EOF) && (function_parse_bytes_read > 0))
-      // {
-      //   Function * function = maze->functions + function_index;
-      //   function->type = FunctionAssignment;
-      //   function->name[0] = cell_str[0];
-      //   function->name[1] = cell_str[1];
+      uint32_t function_index = get_function_index(cell_str);
+      uint32_t function_parse_bytes_read = 0;
 
-      //   bytes_read += function_parse_bytes_read;
-      // }
-      // else
-      // {
-      new_cell.type = CELL_FUNCTION;
-      //   new_cell.data.function_index = function_index;
-      // }
+      char * ptr = f_ptr + 2;
+      consume_whitespace(ptr, f_end);
+
+      if (ptr[0] == '-' &&
+          ptr[1] == '>')
+      {
+        // Function Definition
+        ptr += 2;
+        Function * function = maze->functions + function_index;
+        function->name[0] = cell_str[0];
+        function->name[1] = cell_str[1];
+
+        consume_whitespace(ptr, f_end);
+
+        if (*ptr == '=')
+        {
+          consume_whitespace(ptr, f_end);
+
+          ptr = get_num(ptr, f_end, &(function->value));
+          function->type = FUNCTION_ASSIGNMENT;
+
+          consume_until_newline(ptr, f_end);
+          f_ptr = ptr;
+
+        }
+        else if (*ptr == '+' ||
+                 *ptr == '-' ||
+                 *ptr == '*' ||
+                 *ptr == '/')
+        {
+          char sign = *ptr++;
+          if (*ptr++ == '=')
+          {
+            switch (sign)
+            {
+              case '+':
+              {
+                function->type = FUNCTION_INCREMENT;
+              } break;
+              case '-':
+              {
+                function->type = FUNCTION_DECREMENT;
+              } break;
+              case '*':
+              {
+                function->type = FUNCTION_MULTIPLY;
+              } break;
+              case '/':
+              {
+                function->type = FUNCTION_DIVIDE;
+              } break;
+            }
+            consume_whitespace(ptr, f_end);
+
+            ptr = get_num(ptr, f_end, &(function->value));
+
+            consume_until_newline(ptr, f_end);
+            f_ptr = ptr;
+          }
+
+        }
+        else if (strncmp(ptr, "IF", 2) == 0)
+        {
+          ptr += 2;
+          consume_whitespace(ptr, f_end);
+
+          FunctionType conditional_func_type;
+          bool valid_condition = true;
+          bool value_required = true;
+          if (strncmp(ptr, "<=", 2) == 0)
+          {
+            ptr += 2;
+            conditional_func_type = FUNCTION_LESS_EQUAL;
+          }
+          if (*ptr == '<')
+          {
+            ptr += 1;
+            conditional_func_type = FUNCTION_LESS;
+          }
+          else if (strncmp(ptr, ">=", 2) == 0)
+          {
+            ptr += 2;
+            conditional_func_type = FUNCTION_GREATER_EQUAL;
+          }
+          else if (*ptr == '>')
+          {
+            ptr += 1;
+            conditional_func_type = FUNCTION_GREATER;
+          }
+          else if (strncmp(ptr, "==", 2) == 0)
+          {
+            ptr += 2;
+            conditional_func_type = FUNCTION_EQUAL;
+          }
+          else if (strncmp(ptr, "!=", 2) == 0)
+          {
+            ptr += 2;
+            conditional_func_type = FUNCTION_NOT_EQUAL;
+          }
+          else if (strncmp(ptr, "**", 2) == 0)
+          {
+            ptr += 2;
+            conditional_func_type = FUNCTION_SIGNAL;
+            value_required = false;
+          }
+          else
+          {
+            printf("Conditional \n");
+            valid_condition = false;
+          }
+
+          uint32_t condition_value;
+          if (value_required && valid_condition)
+          {
+            consume_whitespace(ptr, f_end);
+
+            char * num_start = ptr;
+            ptr = get_num(ptr, f_end, &(condition_value));
+            if (num_start == ptr)
+            {
+              valid_condition = false;
+            }
+          }
+
+          if (valid_condition)
+          {
+            consume_whitespace(ptr, f_end);
+
+            if (strncmp(ptr, "THEN", 4) == 0)
+            {
+              ptr += 4;
+              consume_whitespace(ptr, f_end);
+
+              enum Direction true_direction;
+              ptr = get_direction(ptr, &true_direction);
+              if (true_direction != STATIONARY)
+              {
+                consume_whitespace(ptr, f_end);
+
+                if (strncmp(ptr, "ELSE", 4) == 0)
+                {
+                  ptr += 4;
+                  consume_whitespace(ptr, f_end);
+
+                  enum Direction false_direction;
+                  ptr = get_direction(ptr, &false_direction);
+                  if (false_direction != STATIONARY)
+                  {
+                    function->type = conditional_func_type;
+                    function->conditional.condition_value = condition_value;
+                    function->conditional.true_direction = true_direction;
+                    function->conditional.false_direction = false_direction;
+
+                    consume_until_newline(ptr, f_end);
+                    f_ptr = ptr;
+                  }
+                }
+                else
+                {
+                  consume_until_newline(ptr, f_end);
+                  f_ptr = ptr;
+                }
+              }
+            }
+          }
+        }
+      }
+      else
+      {
+        // Funciton Call
+        new_cell.type = CELL_FUNCTION;
+        new_cell.function_index = function_index;
+      }
     }
-    else if ((cell_str[0] == '-') && (cell_str[1] == '-'))
+    else if (strncmp(cell_str, "--", 2) == 0)
     {
       new_cell.type = CELL_ONCE;
     }
@@ -184,7 +420,19 @@ parse(GameMemory * game_memory, const char * filename)
       new_cell.pause = (10 * digit0) + digit1;
     }
 
-    if (new_cell.type == CELL_NULL)
+    if (new_cell.type != CELL_NULL)
+    {
+      Cell * cell = get_cell(maze, x, y, game_memory);
+      cell->type = new_cell.type;
+      cell->pause = new_cell.pause;
+      cell->function_index = new_cell.function_index;
+
+      printf("%s ", cell_str);
+
+      f_ptr += 2;
+      ++x;
+    }
+    else
     {
       if (cell_str[0] == '\n')
       {
@@ -194,20 +442,9 @@ parse(GameMemory * game_memory, const char * filename)
         }
         x = 0;
         ++y;
-        // printf("\n");
+        printf("\n");
       }
       f_ptr += 1;
-    }
-    else
-    {
-      Cell * cell = get_cell(maze, x, y, game_memory);
-      cell->type = new_cell.type;
-      cell->data = new_cell.data;
-
-      f_ptr += 2;
-      ++x;
-
-      // printf("%s ", cell_str);
     }
   }
   maze->height = y;
