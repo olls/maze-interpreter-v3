3bb577783a5421f32ba3d2c48dd815525be9ae7c
Oliver Faircliff <mail@oliverfaircliff.com>
2016-10-05 16:47:02 +0100

    Reduce default window size

--- main.h
+++ main.h
@@ -32,8 +32,8 @@
 
 
 const b32 FULLSCREEN = false;
-const u32 WINDOW_WIDTH = 1920;
-const u32 WINDOW_HEIGHT = 1080;
+const u32 WINDOW_WIDTH = 1280.0/1.2;
+const u32 WINDOW_HEIGHT = 720.0/1.2;
 
 const u32 FPS = 60;
 const u32 TOTAL_MEMORY = megabytes_to_bytes(50);
