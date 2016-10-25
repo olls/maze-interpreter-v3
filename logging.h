8dc7670ac2c6719eac3ad081fe59cea997a2da7a
Oliver Faircliff <mail@oliverfaircliff.com>
2016-10-25 16:10:09 +0100

    Add missing logging.h

---
+++ logging.h
@@ -0,0 +1,29 @@
+#define ALL_LOGGING_CHANNELS(CHANNEL) \
+          CHANNEL(L_Main) \
+          CHANNEL(L_CarsStorage) \
+          CHANNEL(L_CarsSim) \
+          CHANNEL(L_CellsStorage) \
+          CHANNEL(L_Cells) \
+          CHANNEL(L_Parser) \
+          CHANNEL(L_Serializer) \
+          CHANNEL(L_Particles) \
+          CHANNEL(L_UI) \
+          CHANNEL(L_Bitmap) \
+          CHANNEL(L_Font) \
+          CHANNEL(L_Vector) \
+          CHANNEL(L_Render) \
+          CHANNEL(L_RenderQueue) \
+          CHANNEL(L_GameLoop) \
+          CHANNEL(N_LOGGING_CHANNELS)
+
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING,
+
+enum LoggingChannel
+{
+  ALL_LOGGING_CHANNELS(GENERATE_ENUM)
+};
+
+static const char *LOGGING_CHANNELS[] = {
+  ALL_LOGGING_CHANNELS(GENERATE_STRING)
+};
