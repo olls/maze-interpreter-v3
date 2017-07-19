17ae1f41d35f98837e0905e70ea2f38ad0a10da1
Oliver Faircliff <mail@oliverfaircliff.com>
2017-07-19 22:57:17 +0100

    Quadratic bezier subdivide algorithm

---
+++ engine/quadratic-bezier.h
@@ -0,0 +1,15 @@
+const u32 MAX_BEZIER_SUBDIVISIONS = 512;
+const r32 LINE_SEGMENT_FLATNESS_ELIPSON = 0.01;
+
+struct BezierControlPoint
+{
+  vec2 point;
+  b32 on_curve;
+};
+
+
+struct VertexBuffer
+{
+  vec2 *vertices;
+  u32 n_vertices;
+};
